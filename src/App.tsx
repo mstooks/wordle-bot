761dd946e2758e2fd582ad0ffe3b4413bf6f0f79
unknown <mitchell.stooksbury@yahoo.com>
2024-03-10 01:13:59 -0600

    Making finger animation scale a bit more appropriate

--- src/App.tsx
+++ src/App.tsx
@@ -207,14 +207,14 @@ function App() {
                             component="img"
                             src={tappingGif}
                             alt="Tap here"
-                            className="fade-in"
+                            className="fade-in-out"
                             sx={{
                                 position: 'absolute',
                                 top: 'calc(50% + 23px)',
                                 left: '52.5%',
                                 transform: 'translate(-50%, -50%)',
-                                width: '3rem',
-                                height: '3rem',
+                                width: '2.25rem',
+                                height: '2.25rem',
                                 pointerEvents: 'none',
                             }}
                         />
